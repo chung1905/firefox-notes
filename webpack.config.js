1e8f6e9d21a819da4d00ffeef87f848e31f54f9e
chung <me@chungn.com>
2026-02-03 11:04:28 +0700

    chore: migrate Babel 6 to Babel 7
    - Replace babel-core@6 with @babel/core@7.23
    - Replace babel-preset-env/es2015/react/stage-2 with @babel/preset-env and @babel/preset-react
    - Update babel-loader from 7.x to 8.x
    - Add @babel/register for test script compatibility
    - Update .babelrc to use new preset syntax
    - Update webpack.config.js babel-loader configuration
    - Update test:ui script to use @babel/register

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,29 @@
-'use strict';
+"use strict";
 
 /* eslint-env node */
 
-const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  devtool: 'source-map',
+  devtool: "source-map",
 
-  entry: [
-    path.resolve(__dirname, 'src', 'sidebar', 'app', 'app.js'),
-  ],
+  entry: [path.resolve(__dirname, "src", "sidebar", "app", "app.js")],
 
   output: {
     // build to the extension src vendor directory
-    path: path.resolve(__dirname, 'build'),
-    filename: path.join('sidebar', 'app.js'),
+    path: path.resolve(__dirname, "build"),
+    filename: path.join("sidebar", "app.js")
   },
 
   plugins: [
     // Moves files
-    new CopyWebpackPlugin([
-      { from: path.join('src') }
-    ], {
+    new CopyWebpackPlugin([{ from: path.join("src") }], {
       ignore: [
-          path.join('sidebar', 'app', '**', '*'),
-          path.join('sidebar', 'static', 'scss', '**', '*')
-      ],
-    }),
+        path.join("sidebar", "app", "**", "*"),
+        path.join("sidebar", "static", "scss", "**", "*")
+      ]
+    })
   ],
 
   module: {
@@ -35,26 +31,36 @@ module.exports = {
       {
         test: /\.js$/, // Babel-loader compile jsx syntax to javascript
         exclude: /node_modules/,
-        use: ['babel-loader?presets[]=env&presets[]=react&presets[]=stage-2&sourceMaps=true'],
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+            sourceMaps: true
+          }
+        }
       },
       {
-          test: /\.scss$/,
-          use: [{
-              loader: 'style-loader' // creates style nodes from JS strings
-          }, {
-              loader: 'css-loader' // translates CSS into CommonJS
-          }, {
-              loader: 'sass-loader' // compiles Sass to CSS
-          }]
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader" // creates style nodes from JS strings
+          },
+          {
+            loader: "css-loader" // translates CSS into CommonJS
+          },
+          {
+            loader: "sass-loader" // compiles Sass to CSS
+          }
+        ]
       },
       {
         test: /\.(jpe?g|png|gif|svg|eot|woff|ttf|svg|woff2)$/,
         options: {
-          name: '[path][name].[ext]',
-          context: 'src',
-          publicPath: '../'
+          name: "[path][name].[ext]",
+          context: "src",
+          publicPath: "../"
         },
-        loader: 'file-loader'
+        loader: "file-loader"
       }
     ]
   }
